f0a1c59dbe206a30ca03fc2fbb64e026090f14f4
Wayne <wayneck0138@gmail.com>
2023-12-05 22:26:14 +0800

    帳號功能-單元測試

--- src/main/java/com/example/springbootmall/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/springbootmall/dao/impl/UserDaoImpl.java
@@ -23,7 +23,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Integer createUser(UserRegisterRequest userRegisterRequest) {
-        String sql = "INSERT INTO user(email,password,created_date,last_modified_date) " +
+        String sql = "INSERT INTO user (email,password,created_date,last_modified_date) " +
                 "VALUES (:email,:password,:createdDate,:lastModifiedDate)";
         Map<String, Object> map = new HashMap<>();
         map.put("email", userRegisterRequest.getEmail());
