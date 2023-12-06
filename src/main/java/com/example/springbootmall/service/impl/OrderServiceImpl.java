cd16d0f30398917bf7c9cc2ee60a9d51643cfaa2
Wayne <wayneck0138@gmail.com>
2023-12-06 17:22:06 +0800

    訂單功能-檢查user存在與庫存

--- src/main/java/com/example/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.example.springbootmall.service.impl;
 
 import com.example.springbootmall.dao.OrderDao;
 import com.example.springbootmall.dao.ProductDao;
+import com.example.springbootmall.dao.UserDao;
 import com.example.springbootmall.dto.BuyItem;
 import com.example.springbootmall.dto.CreateOrderRequest;
 import com.example.springbootmall.model.Order;
 import com.example.springbootmall.model.OrderItem;
 import com.example.springbootmall.model.Product;
+import com.example.springbootmall.model.User;
 import com.example.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,39 +27,62 @@ public class OrderServiceImpl implements OrderService {
     private OrderDao orderDao;
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    private UserDao userDao;
 
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Override
     @Transactional
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        //檢查user是否存在
+        User user = userDao.getUserById(userId);
+        if (user == null) {
+            log.warn("該user {} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
-        List<OrderItem> orderItemList=new ArrayList<>();
+        List<OrderItem> orderItemList = new ArrayList<>();
 
         //遍歷了 createOrderRequest 中的 buyItemList (List<BuyItem>)
         //BuyItem有productId 和 quantity，價格要去product取得
         for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductId(buyItem.getProductId());
+
+            //檢查product庫存，成功則扣庫存
+            if(product==null){
+                log.warn("商品{}不存在",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } else if (product.getStock()<buyItem.getQuantity()) {
+                //若買兩樣都不足，但僅會出現第一個不足的
+                log.warn("商品{}庫存數量不足，無法購買，剩餘庫存{}，欲購買數量{}",
+                        buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            productDao.updateStock(product.getProductId(),product.getStock()- buyItem.getQuantity());
+
             int amount = buyItem.getQuantity() * product.getPrice();
             totalAmount = totalAmount + amount;
 
             //轉換 buyItem 成 orderItem
-            OrderItem orderItem=new OrderItem();
+            OrderItem orderItem = new OrderItem();
             orderItem.setProductId(buyItem.getProductId());
             orderItem.setQuantity(buyItem.getQuantity());
             orderItem.setAmount(amount);
             orderItemList.add(orderItem);
         }
         //創建訂單
-        Integer orderId = orderDao.createOrder(userId,totalAmount);
+        Integer orderId = orderDao.createOrder(userId, totalAmount);
         //創建明細
-        orderDao.createOrderItems(orderId,orderItemList);
+        orderDao.createOrderItems(orderId, orderItemList);
         return orderId;
     }
 
     @Override
     public Order getOrderById(Integer orderId) {
-        Order order=orderDao.getOrderById(orderId);
-        List<OrderItem> orderItemList=orderDao.getOrderItemsByOrderId(orderId);
+        Order order = orderDao.getOrderById(orderId);
+        List<OrderItem> orderItemList = orderDao.getOrderItemsByOrderId(orderId);
         order.setOrderItemList(orderItemList);
         return order;
     }
